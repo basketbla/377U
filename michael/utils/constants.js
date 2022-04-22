5f32a03f49bd11d5811e988f6151d155ab12fd0c
Rhett <rcowen@stanford.edu>
2022-04-22 11:24:46 -0700

    messing w server

--- michael/utils/constants.js
+++ michael/utils/constants.js
@@ -1,6 +1,6 @@
 
 // Actual server
-// export const NODE_URL = 'https://michael-the-app.herokuapp.com';
+export const NODE_URL = 'https://michael-the-app-server.herokuapp.com';
 
 // For local testing
-export const NODE_URL = 'http://localhost:3001';
+// export const NODE_URL = 'http://localhost:3001';
--- node-server/ecosystem.config.js
+++ node-server/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    script: './server.js',
+    script: './Server.js',
     watch: '.'
   }]
 };
