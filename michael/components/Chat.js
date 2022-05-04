4d7d29aa52a52ffcecff14082cec25fe2b5109d4
David Guo <davidguo@DN0a25a8c6.SUNet>
2022-05-03 19:31:25 -0700

    added basic chat, also commented-out code to connect to firebase + do db auth stuff

--- michael/components/Chat.js
+++ michael/components/Chat.js
@@ -1,13 +1,114 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// import React from 'react'
 
-export default function Chat() {
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+// import db from "../firebase"; //do we have this? 
+import firebase from "@firebase/app";
+
+// export default function App() {
+//   const [messages, setMessages] = useState([]);
+//   const onSend = useCallback((messages = []) => {
+//     setMessages((previousMessages) =>
+//       GiftedChat.append(previousMessages, messages)
+//     );
+//   }, []);
+//   // return (
+//   // );
+// }
+export default function Chat({ route }) {
+  const [messages, setMessages] = useState([]);
+
+  const { chatname } = "hello, world"; //name of the chat group 
+  // console.log(firebase.auth().currentUser);
+
+  // useEffect(() => {
+  //   let unsubscribeFromNewSnapshots = db
+  //     .collection("Chats") //ideally firestore, not realtime db
+  //     .doc(chatname)
+  //     .onSnapshot((snapshot) => {
+  //       console.log("New Snapshot!");
+  //       let newMessages = snapshot.data().messages.map((singleMessage) => {
+  //         singleMessage.createdAt = singleMessage.createdAt.seconds * 1000;
+  //         return singleMessage;
+  //       });
+  //       setMessages(newMessages);
+  //     });
+
+  //   return function cleanupBeforeUnmounting() {
+  //     unsubscribeFromNewSnapshots();
+  //   };
+  // }, []);
+
+  //firebase onsend or non-firebase onsend
+    const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  }, []);
+
+  // const onSend = useCallback((messages = []) => {
+  //   db.collection("Chats")
+  //     .doc(chatname)
+  //     .update({
+  //       // arrayUnion appends the message to the existing array
+  //       messages: firebase.firestore.FieldValue.arrayUnion(messages[0]),
+  //     });
+  //   setMessages((previousMessages) =>
+  //     GiftedChat.append(previousMessages, messages)
+  //   );
+  // }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Chat</Text>
-    </View>
-  )
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)} // can switch out with firebase onSend or non-firebase onSend
+      user={{
+          _id: 1,
+          name: "Jenny",
+          avatar: "https://placeimg.com/140/140/any",
+      }}
+      // user={{
+      //   // current "blue bubble" user
+      //   _id: firebase.auth().currentUser.uid,
+      //   name: firebase.auth().currentUser.displayName,
+      //   avatar: "https://placeimg.com/140/140/any", //need an image "../assets/avatar.png"
+      // }}
+      inverted={false}
+      showUserAvatar={true}
+      renderUsernameOnMessage={true}
+    />
+  );
 }
+
+// export default function Chat() {
+//   const [messages, setMessages] = useState([]);
+
+//   const onSend = useCallback((messages = []) => {
+//     setMessages((previousMessages) =>
+//       GiftedChat.append(previousMessages, messages)
+//     );
+//   }, []);
+//   return (
+//     // <View style={styles.container}>
+//       // <div>
+//         <GiftedChat
+//         messages={messages}
+//         onSend={(messages) => onSend(messages)}
+//         user={{
+//           _id: 1,
+//           name: "Jenny",
+//           avatar: "https://placeimg.com/140/140/any",
+//         }}
+//         showUserAvatar={true}
+//       />
+
+//       // </div>
+//       // {/* <Text>Chat</Text> */}
+
+//     // </View>
+//   )
+// }
 
 const styles = StyleSheet.create({
   container: {
